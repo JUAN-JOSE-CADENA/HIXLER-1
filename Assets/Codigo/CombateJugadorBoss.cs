f6bd8aea7486ff1561701eaaa4e628d6b98ff974
JUAN-JOSE-CADENA <69523656+JUAN-JOSE-CADENA@users.noreply.github.com>
2022-11-17 12:34:55 -0500

    ajustes pequewños

---
+++ Assets/Codigo/CombateJugadorBoss.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CombateJugadorBoss : MonoBehaviour
+{
+
+    [SerializeField] private int vida;
+    [SerializeField] private int maximoVida;
+    [SerializeField] private BarraVida barraDeVida;
+
+    [SerializeField] private AudioClip sonidoGolpe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        vida = maximoVida;
+        barraDeVida.InicializarBarraDeVida(vida);
+
+    }
+
+
+    public void TomarDaño(int daño)
+    {
+        vida -= daño;
+        barraDeVida.CambiarVidaActual(vida);
+        ControladorSonidos.Instance.EjecutarSonido(sonidoGolpe);
+
+        if (vida <= 0)
+        {
+
+            Destroy(gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        }
+    }
+
+
+    public void Curar(int curacion)
+    {
+        if ((vida + curacion) > maximoVida)
+        {
+            vida = maximoVida;
+        }
+
+        else
+        {
+            vida += curacion;
+        }
+    }
+
+}
